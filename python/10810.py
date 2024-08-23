c47911099741ded954ce47cd3d42afe61715c756
Henry Shin <bg22915@bristol.ac.uk>
2024-08-23 20:40:49 +0900

    Update 10810.py

--- python/10810.py
+++ python/10810.py
@@ -4,9 +4,8 @@ box = [0] * n
 
 for a in range(m):
     i, j, k = map(int, input().split())
-    for i in range (i, j):
-        box[i] += k
-
+    for i in range(i, j+1):
+        box[i-1] = k
 for i in range(n):
     box = map(str, box)
 print(" ".join(box))
